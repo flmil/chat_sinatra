31c9cacefeda8a716c3b11714f55fd55471778ef
ojirei <Rei@S-ReiOji-no-MacBook-Pro.local>
2017-02-05 10:35:39 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra'
+require 'sinatra/json'
+require './models.rb'
+require 'open-uri'
+require 'time'
+require './models'
+
+enable :sessions
+
+get '/' do
+	erb :index
+end
+
+get '/signin' do
+	erb :signin
+end
+
+get '/signup' do
+	erb :signup
+end
+
+post '/signup' do
+	@user = User.create(
+		mail:params[:mail],
+		name:params[:name],
+		idsearch:params[:idsearch],
+		password:params[:password],
+		password_confirmation:params[:password_confmation],
+	)
+	if @user.persisted?
+		session[:user] = @user.id
+	end
+	redirect '/'
+end
+
+post '/signin' do
+	user = User.find_by(mail: params[:mail])
+	if user && user.authenticate(params[:passwrod])
+		session[:user] = user.id
+	end
+	redirect '/'
+end
+
+get '/signout' do
+	session[:user] = nil
+	redirect '/'
+end
---
+++ models.rb
@@ -0,0 +1,14 @@
+#require 'bundler/setup'
+#Bundler.require
+
+#if development?
+ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+#end
+
+#unless ENV['RACK_ENV'] == 'production'
+#    ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+#end
+
+class User < ActiveRecord::Base
+	has_secure_password
+end
