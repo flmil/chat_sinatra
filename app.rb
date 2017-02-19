803742bbcfee5f5c81a5d4e111c7570b6ed95b14
ojirei <Rei@S-ReiOji-no-MacBook-Pro.local>
2017-02-19 23:39:39 +0900

    privatechat Addition

--- app.rb
+++ app.rb
@@ -139,15 +139,34 @@ post '/friend/:user_id' do
 	redirect '/list_user'
 end
 
-get  '/oneroom/:user_id' do
+get  '/oneroom/:friend_id' do
+	friend = Relationship.find(params[:friend_id])
 	@user = current_user
 	# TODO:
 	# user_idとcurrent_user.idからRoomを探す
-	# →見つかればそれ使う
-	# なければRoom作る
-	@namerooms = Room.find_by(id: params[:user_id])
-	@message = @namerooms.messages
+	if Acticletag.where(user_id: [friend.user_id, friend.friend_id]).group(:room_id).having('count(*) = 2').exists?
+		# 見つかればそれ使う
+		User.where(user_id: [relationship.user_id, relationship.friend_id]).group(:room_id).each do |room|
+			@room = Room.where(id: room.room_id)
+			redirect "room/#{room.room_id}"
+		end
+		# なければRoom作る
+	else
+		##@room = Room.where(id: room_id)
+		#@room = Room.find_by(id: params[:room_id])
+		#p Acticletag.create(room: @room,user_id: friend.id)
+		#p Acticletag.create(room: @room,user_id: current_user.id)
+		#p redirect "room/#{@room_id}"
+		room = Room.create({roomname: params[:roomname],})
+		Acticletag.create({
+			room_id: room,
+			user_id: session[:user]
+		})
+		redirect "room/#{room_id}"
+	end
+
+	#@namerooms = Room.find_by(id: params[:user_id])
+	#@message = @namerooms.messages
 
 	erb :room
 end
-
